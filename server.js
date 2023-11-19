ffeecca90b3d11bda3d634b1bb03955200b7cffb
katsisaac50 <katsisaac50@gmail.com>
2023-11-19 09:41:16 +0100

    routes created

--- server.js
+++ server.js
@@ -1,37 +1,92 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import morgan from 'morgan';
-import dotenv from 'dotenv';
+// const express = require('express');
+// const cors = require('cors');
+// const mongoose = require('mongoose');
+// const morgan = require('morgan');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
 
+// const app = express();
+
+// const mongodbUri = process.env.MONGODB_URI;
+
+
+// // db connection
+// mongoose.connect(mongodbUri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+//   useCreateIndex: true,
+// })
+//   .then(() => {
+//     console.log('MongoDB connected');
+//   })
+//   .catch((err) => {
+//     console.error('MongoDB connection error:', err);
+//   });
+
+// // middleware
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+// }));
+// app.use(express.json({
+//   limit: '50mb',
+//   type: 'application/json',
+// }));
+// app.use(morgan('dev'));
+// app.use(express.urlencoded({ extended: true }));
+
+// // routes
+// // app.use('/api/v1/auth', require('./routes/auth'));
+// // app.use('/api/v1/users', require('./routes/users'));
+// // app.use('/api/v1/posts', require('./routes/posts'));
+// // app.use('/api/v1/comments', require('./routes/comments'));
+// // app.use('/api/v1/likes', require('./routes/likes'));
+// // app.use('/api/v1/dislikes', require('./routes/dislikes'));
+
+// // routes
+// app.post('/api/register', (req, res) => {
+//   console.log('Register =>', req.body);
+//   res.status(200).json({ message: 'Registration successful' });
+// });
+
+// const port = process.env.PORT || 8006;
+
+// app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const https = require('https');
+const fs = require('fs');
+
+require('dotenv').config();
 const app = express();
+const port = process.env.PORT || 8007;
 
 const mongodbUri = process.env.MONGODB_URI;
 
-if (!mongodbUri) {
-  throw new Error('MONGODB_URI is not defined in the environment variables');
-}
+// Enable CORS
+app.options('*', cors());
 
-// db connection
-mongoose.connect(mongodbUri, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('MongoDB connected');
-  })
-  .catch((err) => {
-    console.error('MongoDB connection error:', err);
-  });
 
-// middleware
 app.use(cors({
   origin: 'http://localhost:3000',
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
 }));
+
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+      console.log('MongoDB connected');
+    })
+    .catch((err) => {
+      console.error('MongoDB connection error:', err);
+    });
+
+
+// Set up middleware for parsing JSON requests
 app.use(express.json({
   limit: '50mb',
   type: 'application/json',
@@ -39,20 +94,18 @@ app.use(express.json({
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 
-// routes
-// app.use('/api/v1/auth', require('./routes/auth'));
-// app.use('/api/v1/users', require('./routes/users'));
-// app.use('/api/v1/posts', require('./routes/posts'));
-// app.use('/api/v1/comments', require('./routes/comments'));
-// app.use('/api/v1/likes', require('./routes/likes'));
-// app.use('/api/v1/dislikes', require('./routes/dislikes'));
+// Set up routes
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
 
-// routes
 app.post('/api/register', (req, res) => {
   console.log('Register =>', req.body);
   res.status(200).json({ message: 'Registration successful' });
 });
 
-const port = process.env.PORT || 8006;
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
