05533c4595d2d4d6bb97feffd67cae98e3be5dbb
katsisaac50 <katsisaac50@gmail.com>
2023-11-21 12:36:42 +0100

    user successfully added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,32 @@
+const User = require('../models/user');
+
 register = async (req, res) => {
 
-    console.log('Register =>', req.body);
+    //  console.log('Register =>', req.body);
+
+    const {name, email, password, repeatPassword,  selectedQuestion, secretAnswer} = req.body;
+
+    console.log(secretAnswer, selectedQuestion, email, name, password, repeatPassword);
+
+    if(!name) return res.status(400).json({ message: 'Name is required' });
+    if (!email) return res
+    .status(400)
+    .json({ message: 'Email is required' });
+    if (!password || password.length < 6) return res
+    .status(400)
+    .json({ message: 'Password is required and should be min 6 characters long' });
+    if (password !== repeatPassword) return res
+    .status(400)
+    .json({ message: 'Passwords do not match' });
+    if (!secretAnswer) return res
+    .status(400)
+    .json({ message: 'Secret is required' });
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res
+    .status(400)
+    .json({ message: 'User already exists' });
+
     res.status(200).json({ message: 'Registration successful' });
 
 }
--- models/user.js
+++ models/user.js
@@ -1,45 +1,42 @@
 const mongoose = require('mongoose');
-const {Schema} = require('mongoose');
 
-const userSchema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 64
-    },
-    secret: {
-        type: String,
-        required: true,
-    },
-    about: {
-        type: String,
-        trim: true
-    },
-    photo: {
-        data: Buffer,
-        contentType: String
-    },
-    following: [{type: Schema.Types.ObjectId, ref: 'User'}],
-    followers: [{type: Schema.Types.ObjectId, ref: 'User'}],
-    resetPasswordLink: {
-        data: String,
-        default: ''
-    },
-
-
-}, {timestamps: true});
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6, // Use minlength instead of min
+    maxlength: 64, // Use maxlength instead of max
+  },
+  secret: {
+    type: String,
+    required: true,
+  },
+  about: {
+    type: String,
+    trim: true,
+  },
+  photo: {
+    data: Buffer,
+    contentType: String,
+  },
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  resetPasswordLink: {
+    data: String,
+    default: String,
+  },
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 
