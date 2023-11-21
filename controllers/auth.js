fed2cbaaf710b64b864a1feff3da338e1074652a
katsisaac50 <katsisaac50@gmail.com>
2023-11-21 16:42:12 +0100

    create a new user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const { hashPassword, comparePassword } = require('../helpers/auth');
 
 register = async (req, res) => {
 
@@ -6,12 +7,19 @@ register = async (req, res) => {
 
     const {name, email, password, repeatPassword,  selectedQuestion, secretAnswer} = req.body;
 
-    console.log(secretAnswer, selectedQuestion, email, name, password, repeatPassword);
+    // console.log(secretAnswer, selectedQuestion, email, name, password, repeatPassword);
 
+    // validations
     if(!name) return res.status(400).json({ message: 'Name is required' });
     if (!email) return res
     .status(400)
     .json({ message: 'Email is required' });
+    if (!selectedQuestion) return res
+    .status(400)
+    .json({ message: 'Question is required' });
+    if (!secretAnswer) return res
+    .status(400)
+    .json({ message: 'Secret is required' });
     if (!password || password.length < 6) return res
     .status(400)
     .json({ message: 'Password is required and should be min 6 characters long' });
@@ -21,13 +29,38 @@ register = async (req, res) => {
     if (!secretAnswer) return res
     .status(400)
     .json({ message: 'Secret is required' });
-
     const existingUser = await User.findOne({ email });
     if (existingUser) return res
     .status(400)
     .json({ message: 'User already exists' });
 
-    res.status(200).json({ message: 'Registration successful' });
+    // hash password
+    const hashedPassword = await hashPassword(password);
+
+    // create new user
+    const user = await new User({
+        name,
+        email,
+        selectedQuestion,
+        secretAnswer,
+        password: hashedPassword
+    }).save();
+
+    try {
+        await user.save();
+        console.log("register success =>", user);
+        return res.status(200).json(
+            { 
+            success: true, 
+            message: 'Registration successful' 
+            });
+    } catch (error) {
+        console.log("register failed =>", error);
+        return res.status(400).json(
+            { 
+            message: 'Registration failed' 
+            });
+    }
 
 }
 
